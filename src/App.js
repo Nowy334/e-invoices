2b9115a46cdc1648db0401491623736e62be9b2e
Nowy334 <krzysnowaks@gmail.com>
2021-05-19 21:58:23 +0200

    creating and handling a GET query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import './App.module.scss';
 import Sidebar from './components/Sidebar/Sidebar';
 import SettingsPage from './components/SettingsPage/SettingsPage';
@@ -10,6 +10,42 @@ function App() {
   const[isToggled, setIsToggled] = useState(false);
   const[modalIsShow, setModalIsShown] = useState(false);
   const [pageWidth, setPageWidth] = useState(window.innerWidth);
+  const [service, setService] = useState(false);
+
+
+  const fetchServiceHandler = useCallback(async() => {
+
+    try{
+      const response = await fetch('http://api.nbp.pl/api/exchangerates/rates/a/usd?format=json');
+
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
+      const hundredthPart = data.rates[0].mid.toString();
+
+      if(+hundredthPart[4] % 2 === 0){
+        setService(true);
+      }else{
+        setService(false);
+      }
+
+      if(service){
+        console.log('the service is active');
+      }else {
+        console.log('the service is not active');
+      }
+      
+    }catch(error){
+      console.log(error.message)
+    }
+  }, [service]);
+
+  useEffect(() => {
+    if(modalIsShow){
+      fetchServiceHandler();
+    }
+  }, [modalIsShow, fetchServiceHandler]);
 
   const isMobile = pageWidth <= 730;
 
@@ -58,6 +94,8 @@ function App() {
       </>
     );
   }
+
+  //http://api.nbp.pl/api/exchangerates/rates/a/usd?format=json
 
 
   return (
